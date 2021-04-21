96694e311b5188bcf2bb64f455598f0e0f34236c
abdallahelmesery213 <abdallahelmesery@gmail.com>
2021-04-21 14:51:25 +0200

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Form = (props) => {    // just show
+    return ( 
+        <form onSubmit={props.getWeather}>
+            <input type="text" placeholder="City..." name="city" />
+            <input type="text" placeholder="Country..." name="country" />
+            <button>Get Weather</button>
+        </form>
+    );
+}
+
+export default Form;
